e5f752352290b7e4634538966a5e654ff9c9db80
Allen Guo <guosonglin001@gmail.com>
2025-02-22 13:13:03 +0000

    wip

--- _demos/t1.demo.sh
+++ _demos/t1.demo.sh
@@ -23,3 +23,17 @@ source gpu-simulator/gpgpu-sim_distribution/setup &&
 util/job_launching/job_status.py -N rodinia-sass-test
 
 ###############################################################################
+
+args=(
+  -B rodinia_2.0-ft
+  -C QV100-PTX
+  -N rodinia-ptx-test
+  # downloaded data
+  -T _demos/data/hw_run/rodinia_2.0-ft/9.1
+)
+source gpu-simulator/gpgpu-sim_distribution/setup &&
+  util/job_launching/run_simulations.py "${args[@]}"
+
+util/job_launching/job_status.py -N rodinia-ptx-test
+
+###############################################################################
